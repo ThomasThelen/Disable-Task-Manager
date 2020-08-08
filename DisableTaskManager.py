8e4128589a378655ecdb2e9d25e7ea60bc277239
Thomas Thelen <tommythelen@gmail.com>
2020-08-07 20:38:22 -0700

    Add type annotations

--- DisableTaskManager.py
+++ DisableTaskManager.py
@@ -3,17 +3,14 @@ import winreg
 
 def disable_task_manager():
     # Path to the explorer properties
-    registry_path = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+    registry_path: str = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
     # Name of the key
-    registry_name = "DisableTaskMgr"
+    registry_name: str = "DisableTaskMgr"
     # Value that the registry key is set to
-    value = 1
+    value: int = 1
     try:
         reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, registry_path, 0, winreg.KEY_SET_VALUE)
         winreg.SetValueEx(reg_key, registry_name, 0, winreg.REG_SZ, value)
         winreg.CloseKey(reg_key)
     except WindowsError as e:
-        print(e)
-
-
-disable_task_manager()
+        print("There was an error setting the registry key {}.format(e))
